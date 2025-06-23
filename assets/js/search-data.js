13126447c5a8cbfe7d52a7b6f395a002fb178d79
TungHawk <121991422+TungHawk@users.noreply.github.com>
2025-06-23 05:49:10 +0000

    Deploying to gh-pages from @ TungHawk/tunghawk.github.io@73e630f7c5493a0d89544226b29cd40d21ddff02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -471,13 +471,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/nttughawk", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
